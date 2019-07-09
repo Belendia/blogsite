dc01caa6c0d1592ee3cc29ee3fa6cd28fc79dcb5
Belendia Abdissa <belendia@gmail.com>
2019-07-09 14:13:05 +0300

    Add views to different pages

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(defalut=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
 
     # The post might be in draft mode and have no published date. But when you deside to
--- blogsite/urls.py
+++ blogsite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # r'' is to means anything that is not admin, go to blog.urls app
+    url(r'', include('blog.urls'))
 ]
