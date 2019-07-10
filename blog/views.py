471b5801f2300ef1a7d4c31bc89aa5d0f640eb6f
Belendia Abdissa <belendia@gmail.com>
2019-07-10 13:46:26 +0300

    Fill out the view and templates

--- blog/views.py
+++ blog/views.py
@@ -60,7 +60,7 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     # we want to return all the publication dates that don't have publication date
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('crated_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
 
 
 #########################################
@@ -79,7 +79,7 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/comment_form.htmnl', {'form': form})
+    return render(request, 'blog/comment_form.html', {'form': form})
 
 
 @login_required
